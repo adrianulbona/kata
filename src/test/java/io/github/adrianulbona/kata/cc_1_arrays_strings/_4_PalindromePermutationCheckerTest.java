fed348280dc4b08831d2b119aab236f35fbef760
Adrian Bona <adrianulbona@gmail.com>
2019-07-14 15:49:11 +0200

    Add cc 1.4 solution

---
+++ src/test/java/io/github/adrianulbona/kata/cc_1_arrays_strings/_4_PalindromePermutationCheckerTest.java
@@ -0,0 +1,38 @@
+package io.github.adrianulbona.kata.cc_1_arrays_strings;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Predicate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class _4_PalindromePermutationCheckerTest {
+
+    private Predicate<String> checker;
+
+    @BeforeEach
+    void setUp() {
+        this.checker = new _4_PalindromePermutationChecker();
+    }
+
+    @Test
+    void testEmpty() {
+        assertTrue(this.checker.test(""));
+    }
+
+    @Test
+    void testNotPalindrome() {
+        assertFalse(this.checker.test("abc"));
+    }
+
+    @Test
+    void testPalindrome() {
+        assertTrue(this.checker.test("ab  a"));
+    }
+
+    @Test
+    void testPalindromeThreeSpaces() {
+        assertTrue(this.checker.test("ab   a"));
+    }
+}
