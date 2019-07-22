3d34290b5502d28554f694142df3a16d638b801d
Adrian Bona <adrianulbona@gmail.com>
2019-07-22 18:31:48 +0200

    Add cc 1.9 solution

---
+++ src/test/java/io/github/adrianulbona/kata/cc_1_arrays_strings/_09_RotatedStringCheckerTest.java
@@ -0,0 +1,38 @@
+package io.github.adrianulbona.kata.cc_1_arrays_strings;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.BiPredicate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class _09_RotatedStringCheckerTest {
+
+    private BiPredicate<String, String> checker;
+
+    @BeforeEach
+    void setUp() {
+        this.checker = new _09_RotatedStringChecker();
+    }
+
+    @Test
+    void testEmpty() {
+        assertTrue(this.checker.test("", ""));
+    }
+
+    @Test
+    void testRotated() {
+        assertTrue(this.checker.test("bottlewatter", "terbottlewat"));
+    }
+
+    @Test
+    void testNotRotated() {
+        assertFalse(this.checker.test("bottlewatter", "terbottlewah"));
+    }
+
+    @Test
+    void testDifferentLenghts() {
+        assertFalse(this.checker.test("bottlewatteraaaa", "terbottlewat"));
+    }
+}
