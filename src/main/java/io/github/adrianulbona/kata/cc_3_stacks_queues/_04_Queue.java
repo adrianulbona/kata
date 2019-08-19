4305aa43375a09298aae5c9260aca7e84b2dbdc8
Adrian Bona <adrianulbona@gmail.com>
2019-08-19 19:11:40 +0200

    Add cc 3.4 solution

---
+++ src/main/java/io/github/adrianulbona/kata/cc_3_stacks_queues/_04_Queue.java
@@ -0,0 +1,42 @@
+package io.github.adrianulbona.kata.cc_3_stacks_queues;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+class _04_Queue<E> implements Queue<E> {
+
+    private final Stack<E> stack;
+    private final Stack<E> buffer;
+
+
+    static <E> _04_Queue<E> create() {
+        return new _04_Queue<>(StackImpl.create(), StackImpl.create());
+    }
+
+    @Override
+    public void add(E e) {
+        this.stack.push(e);
+    }
+
+    @Override
+    public E poll() {
+        moveAll(this.stack, this.buffer);
+        final E peek = this.buffer.pop();
+        moveAll(this.buffer, this.stack);
+        return peek;
+    }
+
+    @Override
+    public E peek() {
+        moveAll(this.stack, this.buffer);
+        final E peek = this.buffer.peek();
+        moveAll(this.buffer, this.stack);
+        return peek;
+    }
+
+    private void moveAll(Stack<E> from, Stack<E> to) {
+        while (!from.isEmpty()) {
+            to.push(from.pop());
+        }
+    }
+}
---
+++ src/test/java/io/github/adrianulbona/kata/cc_3_stacks_queues/_04_QueueTest.java
@@ -0,0 +1,27 @@
+package io.github.adrianulbona.kata.cc_3_stacks_queues;
+
+import org.junit.jupiter.api.Test;
+
+import static java.util.stream.IntStream.rangeClosed;
+import static org.junit.jupiter.api.Assertions.*;
+
+class _04_QueueTest {
+
+    @Test
+    void testPeek() {
+        final _04_Queue<Integer> queue = _04_Queue.create();
+
+        rangeClosed(1, 5).forEach(i -> {
+            queue.add(i);
+            assertEquals(1, queue.peek());
+        });
+    }
+
+    @Test
+    void testPoll() {
+        final _04_Queue<Integer> queue = _04_Queue.create();
+        rangeClosed(1, 5).forEach(queue::add);
+
+        rangeClosed(1, 5).forEach(i -> assertEquals(i, queue.poll()));
+    }
+}
