148291bf6ad8a85817ffec9d6cdb77f4d02884bd
Adrian Bona <adrianulbona@gmail.com>
2019-12-15 19:11:18 +0100

    Add cc 4.10 solution

---
+++ src/main/java/io/github/adrianulbona/kata/cc_4_graphs_trees/_10_SubTreeChecker.java
@@ -0,0 +1,31 @@
+package io.github.adrianulbona.kata.cc_4_graphs_trees;
+
+import java.util.function.BiPredicate;
+
+import static io.github.adrianulbona.kata.cc_4_graphs_trees.Node.NIL;
+
+public class _10_SubTreeChecker implements BiPredicate<Node, Node> {
+
+    @Override
+    public boolean test(Node potentialChild, Node tree) {
+        if (NIL.equals(tree)) {
+            return false;
+        }
+
+        if (!potentialChild.value().equals(tree.value())) {
+            return test(potentialChild, tree.left()) || test(potentialChild, tree.right());
+        }
+
+        return same(potentialChild, tree);
+    }
+
+    private boolean same(Node node, Node otherNode) {
+        if (NIL.equals(node) && NIL.equals(otherNode)) {
+            return true;
+        }
+
+        return node.value().equals(otherNode.value())
+                && same(node.left(), otherNode.left())
+                && same(node.right(), otherNode.right());
+    }
+}
