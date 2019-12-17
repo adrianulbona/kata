85cca74d98b0df7aadf75d1d90d7f639789700e8
Adrian Bona <adrianulbona@gmail.com>
2019-12-17 14:44:44 +0100

    Add cc 5.03 solution

---
+++ src/test/java/io/github/adrianulbona/kata/cc_5_bits/_03_BitFlipperTest.java
@@ -0,0 +1,24 @@
+package io.github.adrianulbona.kata.cc_5_bits;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class _03_BitFlipperTest {
+
+    @Test
+    void applySingleGaps() {
+        final Function<Integer, Integer> flipper = new _03_BitFlipper();
+
+        assertEquals(8, flipper.apply(0b1110_1110_1111));
+    }
+
+    @Test
+    void applyDoubleGap() {
+        final Function<Integer, Integer> flipper = new _03_BitFlipper();
+
+        assertEquals(7, flipper.apply(0b1110_1110_0111));
+    }
+}
