37bf7734ccfbe438a1663b70026489644a6914e0
Adrian Bona <adrianulbona@gmail.com>
2019-12-22 17:49:58 +0100

    Beautify cc 8.02 solution

--- src/main/java/io/github/adrianulbona/kata/cc_08_recursion_dynamic/_02_RobotWalker.java
+++ src/main/java/io/github/adrianulbona/kata/cc_08_recursion_dynamic/_02_RobotWalker.java
@@ -2,11 +2,10 @@ package io.github.adrianulbona.kata.cc_08_recursion_dynamic;
 
 import lombok.Data;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static io.github.adrianulbona.kata.cc_08_recursion_dynamic._02_RobotWalker.Coord;
@@ -15,38 +14,23 @@ import static java.util.stream.Collectors.toList;
 import static java.util.stream.Stream.concat;
 import static java.util.stream.Stream.of;
 
-class _02_RobotWalker implements Function<Grid, List<Coord>> {
+class _02_RobotWalker implements Function<Grid, Set<List<Coord>>> {
 
     @Override
-    public List<Coord> apply(Grid grid) {
-        final HashMap<Coord, List<Coord>> start = new HashMap<>() {{
-            put(Coord.of(0, 0), List.of(Coord.of(0, 0)));
-        }};
-        return explore(grid, start)
-                .getOrDefault(grid.destination(), List.of());
+    public Set<List<Coord>> apply(Grid grid) {
+        return explore(List.of(Coord.coord(0, 0)), grid)
+                .collect(Collectors.toSet());
     }
 
-    private Map<Coord, List<Coord>> explore(Grid grid, Map<Coord, List<Coord>> paths) {
-        if (paths.keySet().contains(grid.destination())) {
-            return paths;
+    private Stream<List<Coord>> explore(List<Coord> path, Grid grid) {
+        final Coord currentCell = path.get(path.size() - 1);
+        if (currentCell.equals(grid.destination())) {
+            return Stream.of(path);
         }
 
-        final Map<Coord, List<Coord>> newPaths = new HashMap<>(paths);
-        paths.forEach((key, value) -> {
-            final List<Coord> neighbours = grid.explore(key);
-            neighbours.stream()
-                    .filter(neighbour -> !paths.containsKey(neighbour))
-                    .forEach(neighbour -> {
-                        final List<Coord> newPath = concat(value.stream(), of(neighbour)).collect(toList());
-                        newPaths.putIfAbsent(neighbour, newPath);
-                        final List<Coord> existingPath = newPaths.get(neighbour);
-                        if (existingPath.size() > newPath.size()) {
-                            newPaths.put(neighbour, newPath);
-                        }
-                    });
-        });
-
-        return explore(grid, newPaths);
+        return grid.explore(currentCell).stream()
+                .map(neighbour -> concat(path.stream(), of(neighbour)).collect(toList()))
+                .flatMap(extendedPath -> explore(extendedPath, grid));
     }
 
     @Data
@@ -55,16 +39,16 @@ class _02_RobotWalker implements Function<Grid, List<Coord>> {
         private final int row;
         private final int col;
 
-        static Coord of(int row, int col) {
+        static Coord coord(int row, int col) {
             return new Coord(row, col);
         }
 
         Coord down() {
-            return of(this.row + 1, this.col);
+            return coord(this.row + 1, this.col);
         }
 
         Coord right() {
-            return of(this.row, this.col + 1);
+            return coord(this.row, this.col + 1);
         }
 
     }
@@ -81,12 +65,12 @@ class _02_RobotWalker implements Function<Grid, List<Coord>> {
         }
 
         Coord destination() {
-            return Coord.of(rows - 1, columns - 1);
+            return Coord.coord(rows - 1, columns - 1);
         }
 
         List<Coord> explore(Coord coord) {
             if (!valid(coord)) {
-                return List.of();
+                throw new IllegalArgumentException();
             }
 
             return Stream.of(coord.down(), coord.right())
--- src/test/java/io/github/adrianulbona/kata/cc_08_recursion_dynamic/_02_RobotWalkerTest.java
+++ src/test/java/io/github/adrianulbona/kata/cc_08_recursion_dynamic/_02_RobotWalkerTest.java
@@ -2,36 +2,46 @@ package io.github.adrianulbona.kata.cc_08_recursion_dynamic;
 
 import io.github.adrianulbona.kata.cc_08_recursion_dynamic._02_RobotWalker.Grid;
 import io.github.adrianulbona.kata.cc_08_recursion_dynamic._02_RobotWalker.Coord;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Set;
 import java.util.function.Function;
 
+import static io.github.adrianulbona.kata.cc_08_recursion_dynamic._02_RobotWalker.Coord.coord;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class _02_RobotWalkerTest {
 
+    private Function<Grid, Set<List<Coord>>> walker;
+
+    @BeforeEach
+    void setUp() {
+        this.walker = new _02_RobotWalker();
+    }
+
     @Test
     void apply1x1Grid() {
-        final Function<Grid, List<Coord>> walker = new _02_RobotWalker();
         final Grid emptyGrid = Grid.of(1, 1, Set.of());
-        assertEquals(List.of(Coord.of(0,0)), walker.apply(emptyGrid));
+
+        assertEquals(Set.of(List.of(coord(0,0))), this.walker.apply(emptyGrid));
     }
 
     @Test
     void apply2x2Grid() {
-        final Function<Grid, List<Coord>> walker = new _02_RobotWalker();
         final Grid grid = new Grid(2, 2, Set.of());
-        final List<Coord> path = List.of(Coord.of(0, 0), Coord.of(1, 0), Coord.of(1, 1));
-        assertEquals(path, walker.apply(grid));
+        final List<Coord> firstPath = List.of(coord(0, 0), coord(1, 0), coord(1, 1));
+        final List<Coord> secondPath = List.of(coord(0, 0), coord(0, 1), coord(1, 1));
+
+        assertEquals(Set.of(firstPath, secondPath), this.walker.apply(grid));
     }
 
     @Test
     void apply2x2GridWithOffLimitCell() {
-        final Function<Grid, List<Coord>> walker = new _02_RobotWalker();
-        final Grid grid = new Grid(2, 2, Set.of(Coord.of(1, 0)));
-        final List<Coord> path = List.of(Coord.of(0, 0), Coord.of(0, 1), Coord.of(1, 1));
-        assertEquals(path, walker.apply(grid));
+        final Grid grid = new Grid(2, 2, Set.of(coord(1, 0)));
+        final List<Coord> path = List.of(coord(0, 0), coord(0, 1), coord(1, 1));
+
+        assertEquals(Set.of(path), this.walker.apply(grid));
     }
 }
