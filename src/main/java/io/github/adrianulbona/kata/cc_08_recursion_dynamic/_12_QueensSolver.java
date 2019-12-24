2645b0cc7e6f3f4cc058a40c293046cb1dbdc0ca
Adrian Bona <adrianulbona@gmail.com>
2019-12-24 18:40:40 +0200

    Improve cc 8.12 solution

--- src/main/java/io/github/adrianulbona/kata/cc_08_recursion_dynamic/_12_QueensSolver.java
+++ src/main/java/io/github/adrianulbona/kata/cc_08_recursion_dynamic/_12_QueensSolver.java
@@ -1,14 +1,12 @@
 package io.github.adrianulbona.kata.cc_08_recursion_dynamic;
 
 import java.util.List;
-import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
 import static io.github.adrianulbona.kata.cc_08_recursion_dynamic.Coord.coord;
 import static java.lang.Math.abs;
 import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toSet;
 import static java.util.stream.IntStream.range;
 import static java.util.stream.Stream.concat;
 import static java.util.stream.Stream.of;
@@ -24,19 +22,24 @@ public class _12_QueensSolver implements Function<List<Coord>, Stream<List<Coord
                 .flatMap(candidate -> apply(extend(queens, candidate)));
     }
 
-    private List<Coord> extend(List<Coord> queens, Coord candidate) {
-        return concat(queens.stream(), of(candidate)).collect(toList());
-    }
-
     private Stream<Coord> candidates(List<Coord> queens) {
-        final Set<Integer> cols = queens.stream().map(Coord::col).collect(toSet());
         return range(0, 8)
                 .mapToObj(col -> coord(queens.size(), col))
-                .filter(candidate -> !cols.contains(candidate.col()) && !diagonalConflict(queens, candidate));
+                .filter(candidate -> !conflict(queens, candidate));
     }
 
-    private boolean diagonalConflict(List<Coord> queens, Coord candidate) {
+    private boolean conflict(List<Coord> queens, Coord candidate) {
         return queens.stream()
-                .anyMatch(queen -> abs(candidate.row() - queen.row()) == abs(candidate.col() - queen.col()));
+                .anyMatch(queen -> attacking(queen, candidate));
+    }
+
+    private boolean attacking(Coord queen, Coord otherQueen) {
+        return queen.row() == otherQueen.row()
+                || queen.col() == otherQueen.col()
+                || (abs(queen.row() - otherQueen.row()) == abs(queen.col() - otherQueen.col()));
+    }
+
+    private List<Coord> extend(List<Coord> queens, Coord candidate) {
+        return concat(queens.stream(), of(candidate)).collect(toList());
     }
 }
